8d153ee5ae2906be841ffe3563f41d9281a1c11d
Levi <levi-sr@outlook.com>
2020-09-28 22:27:42 -0300

    Initial Structure

---
+++ src/components/Video/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Video extends React.Component {
+    
+
+    render() {
+        return (
+            <div>
+                <strong>Módulo X</strong>
+                <span>Aula X</span>
+            </div>
+        )
+    }
+}
